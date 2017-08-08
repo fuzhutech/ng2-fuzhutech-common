65e4685112f573face99ebb0ecc955bf891b73aa
fuzhutech <fuzhutech@163.com>
2017-08-08 17:39:41 +0800

    当前URL通过window.location获取

--- src/lib/utils/constant.ts
+++ src/lib/utils/constant.ts
@@ -1,2 +1,4 @@
-export const HOST_PATH = 'http://localhost';
-export const HOST_API_PATH = 'http://localhost/api';
+//export const HOST_PATH = 'http://localhost';
+//export const HOST_API_PATH = 'http://localhost/api';
+export const HOST_PATH = window.location.origin;
+export const HOST_API_PATH = HOST_PATH + '/api';
